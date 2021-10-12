73029311279ac9b5b5a5c734ab1a4afdb2e0b67a
Daniel Miller <millerpils@gmail.com>
2021-10-12 16:19:41 +0100

    Dir

---
+++ week_1/objects.js
@@ -0,0 +1,30 @@
+const cryptoCurrencies = [
+  {
+    name: 'Bitcoin',
+    price: 49000,
+    circulatingSupply: 2_100_000,
+    marketCap: function () {
+      return this.price * this.circulatingSupply;
+    },
+  },
+  {
+    name: 'Ethereum',
+    price: 2200,
+    circulatingSupply: 2_000_000,
+    marketCap: function () {
+      return this.price * this.circulatingSupply;
+    },
+  },
+];
+
+const childCoins = [
+  {
+    name: 'Uniswap',
+    price: 2200,
+    circulatingSupply: 2_000_000,
+    marketCap: function () {
+      return this.price * this.circulatingSupply;
+    },
+    parentCoin: cryptoCurrencies[1],
+  },
+];
---
+++ week_1/ways_to_write_functions.js
@@ -0,0 +1,11 @@
+// different ways to write a function
+
+const myFunction1 = () => 'string'; // implied return (introduced in es6 2015)
+
+const myFunction2 = function () {
+  return 'string'; // normal return
+};
+
+function myfunction3() {
+  return 'string';
+}
