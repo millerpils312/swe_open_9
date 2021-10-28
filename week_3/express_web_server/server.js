6ecc313b47b7fc0cb4e4fc3028154a199a19134b
Daniel Miller <millerpils@gmail.com>
2021-10-28 10:38:18 +0100

    Dynamic params

--- week_3/express_web_server/server.js
+++ week_3/express_web_server/server.js
@@ -13,9 +13,27 @@ app.get('/', (request, response) => {
 
 // handle http://localhost:3001/restaurant/1 - dynamic route parameters
 app.get('/restaurant/:id', (request, response) => {
+  // :id = a dynamic (changeable) parameter
+
   response.send(
     `Hello, you asked for the crypto with an ID of ${request.params.id}`
   );
+});
+
+// handle http://localhost:3001/crypto/1 - dynamic route parameters
+app.get('/crypto/:id', (request, response) => {
+  // mock fetch crypto id from database
+
+  if (request.params.id === '1') {
+    obj = {
+      id: request.params.id,
+      name: 'Bitcoin',
+    };
+  } else {
+    obj = 'Some other coin';
+  }
+
+  response.send(obj);
 });
 
 // listen for connections on port 3001 - this starts the server
