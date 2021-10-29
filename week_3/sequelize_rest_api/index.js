b981385d854e0a8a0fd9c73baf91f50d982ef459
Daniel Miller <millerpils@gmail.com>
2021-10-29 15:09:30 +0100

    Remove app app

--- week_3/sequelize_rest_api/index.js
+++ week_3/sequelize_rest_api/index.js
@@ -12,39 +12,42 @@ const port = 3002;
 // support req.body parsing
 app.use(express.json());
 
-app.post('/api/restaurants', async (req, res) => {
-  try {
-    // create a row in the database using sequelize create method
-    const restaurant = await Restaurant.create(req.body);
+app
+  .post('/api/restaurants', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const restaurant = await Restaurant.create(req.body);
 
-    // 201 = created a resource
-    res.status(201).send(restaurant);
-  } catch (e) {
-    res.status(400).send(e.message);
-  }
-});
+      // 201 = created a resource
+      res.status(201).send(restaurant);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  })
 
-app.get('/api/restaurants', async (req, res) => {
-  try {
-    // create a row in the database using sequelize create method
-    const restaurants = await Restaurant.findAll({});
+  .get('/api/restaurants', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const restaurants = await Restaurant.findAll({});
 
-    // 200 = success
-    res.status(200).send(restaurants);
-  } catch (e) {
-    res.status(400).send(e.message);
-  }
-});
+      // 200 = success
+      res.status(200).send(restaurants);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
 
-// 1. create an endpoint that will delete a restaurant by ID (HTTP Method = delete)
+// 1. create an endpoint that will get a restaurant by ID (HTTP Method = get)
 
-// 2. create an endpoint that will update a restaurant by ID (HTTP Method = put)
+// 2. create an endpoint that will delete a restaurant by ID (HTTP Method = delete)
 
-// 3. create a suite of menu and menu item routes that will CRUD each resource
+// 3. create an endpoint that will update a restaurant by ID (HTTP Method = put)
 
-// 4. find a way to relate the menu items to the menu and the menu to the restaurant
+// 4. create a suite of menu and menu item routes that will CRUD each resource
 
-// 5. use Sequelize validation to validate the data being sent (you'll do this in the model)
+// 5. find a way to relate the menu items to the menu and the menu to the restaurant
+
+// 6. use Sequelize validation to validate the data being sent (you'll do this in the model). Which status code would you send back?
 
 /**
  * Synchronize all models with db
